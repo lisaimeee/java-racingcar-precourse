fd4cd03b6b3598a4351f61b2018b931333ccabb3
lisam <lisam333@gmail.com>
2021-05-04 21:25:28 +0900

    feat(RandomNumber): 1에서 9사이의 랜덤값을 구하는 RandomNumber클래스 추가

---
+++ src/main/java/RandomNumber.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class RandomNumber {
+
+    private static final Random random = new Random();
+
+    private static final int BOUND_NINE = 9;
+
+    private RandomNumber() {
+
+    }
+
+    public static int random1To9Number() {
+        return random.nextInt(BOUND_NINE) + 1;
+    }
+}
