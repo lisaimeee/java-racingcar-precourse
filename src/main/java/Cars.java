f261706ed0c74d7dba2bb0f78611b10247daf17f
lisam <lisam333@gmail.com>
2021-05-04 22:10:02 +0900

    feat(Cars): 자동차 이름은 쉼표(,)를 기준으로 구분한다

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -1,16 +1,42 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
 
-    private final List<Car> racingCars;
+    public final String NAME_DELIMITER = ",";
 
     private final int playTurns;
+
+
+    private final List<Car> racingCars;
+
+    public Cars(String names, int playTurns) {
+        this.racingCars = createCars(names, playTurns);
+        this.playTurns = playTurns;
+    }
 
     protected Cars(List<Car> racingCars, int playTurns) {
         this.racingCars = racingCars;
         this.playTurns = playTurns;
+    }
+
+    private List<Car> createCars(String names, int playTurns) {
+        return Arrays.stream(names.split(NAME_DELIMITER))
+                     .map(name -> new Car(name, playTurns))
+                     .collect(Collectors.toList());
+    }
+
+    public void race() {
+        for (int i = 0; i < playTurns; i++) {
+            racingCars.forEach(Car::play);
+        }
+    }
+
+    public List<Car> racingCars() {
+        return Collections.unmodifiableList(racingCars);
     }
 
     public List<Car> winners() {
--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,8 +16,15 @@ public class CarsTest {
         Car winner2 = new Car("win2", playTurns, 3);
         Car player1 = new Car("play1", playTurns, 0);
         Cars cars = new Cars(Arrays.asList(winner1, winner2, player1), playTurns);
-        assertThat(cars.winners()).isNotEmpty()
-                                  .hasSize(2)
-                                  .contains(winner1, winner2);
+        assertThat(cars.winners()).isNotEmpty().hasSize(2).contains(winner1, winner2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5=5", "a,b,c=3"}, delimiter = '=')
+    void 자동차_이름은_쉽표를_기준으로_구분한다(String csv, int size) {
+        int playTurns = 1;
+        Cars cars = new Cars(csv, playTurns);
+        assertThat(cars.racingCars())
+                .hasSize(size);
     }
 }
