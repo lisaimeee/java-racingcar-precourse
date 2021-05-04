c8f456afe10866014e0aaf3492bbaa799a8b5cf4
lisam <lisam333@gmail.com>
2021-05-04 21:00:18 +0900

    feat(Car): 주어진 횟수동안만 움직이는 기능 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,16 +4,31 @@ public class Car {
 
     private static final int MAX_NAME_LENGTH = 5;
 
+    private static final int EXHAUSTED_PLAY_TURN = 0;
+
     public final String name;
 
-    public Car(String name) {
+    public int playTurns;
+
+    public Car(String name, int playTurns) {
         isValidName(name);
         this.name = name;
+        this.playTurns = playTurns;
     }
 
     private void isValidName(String name) {
-        if(Objects.isNull(name) || name.length() > MAX_NAME_LENGTH ){
+        if (Objects.isNull(name) || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("5자리 이하의 이름을 입력해 주세요.");
+        }
+    }
+
+    public void play() {
+        isAbleToPlay();
+    }
+
+    private void isAbleToPlay() {
+        if (playTurns == EXHAUSTED_PLAY_TURN) {
+            throw new IllegalStateException("더이상 움직일 수 없습니다.");
         }
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -2,19 +2,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class CarTest {
 
     @Test
     void 각_자동차에_이름을_부여할_수_있다() {
         String name = "name";
-        Car car = new Car(name);
+        int playTurns = 1;
+        Car car = new Car(name, playTurns);
         assertThat(car.name).isEqualTo(name);
     }
 
     @Test
     void 이름은_5자_이하만_가능하다() {
         String 여섯자리_이름 = "123456";
-        assertThatIllegalArgumentException().isThrownBy(() -> new Car(여섯자리_이름));
+        int playTurns = 1;
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(여섯자리_이름, playTurns));
+    }
+
+    @Test
+    void 자동차는_주어진_횟수동안_움직일_수_있다() {
+        String name = "name";
+        int playTurns = 0;
+        Car car = new Car(name, playTurns);
+        assertThatIllegalStateException().isThrownBy(car::play);
     }
 }
