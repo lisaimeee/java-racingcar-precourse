760607ff7c85a11344160aa2f9253af53324c0b8
lisam <lisam333@gmail.com>
2021-05-04 20:48:53 +0900

    feat(Car): 자동차 이름은 5자리 이하만 가능하다.

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,8 +1,19 @@
+import java.util.Objects;
+
 public class Car {
+
+    private static final int MAX_NAME_LENGTH = 5;
 
     public final String name;
 
     public Car(String name) {
+        isValidName(name);
         this.name = name;
+    }
+
+    private void isValidName(String name) {
+        if(Objects.isNull(name) || name.length() > MAX_NAME_LENGTH ){
+            throw new IllegalArgumentException("5자리 이하의 이름을 입력해 주세요.");
+        }
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,14 +1,20 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
 
     @Test
-    void 각_자동차에_이름을_부여할_수_있다(){
+    void 각_자동차에_이름을_부여할_수_있다() {
         String name = "name";
         Car car = new Car(name);
         assertThat(car.name).isEqualTo(name);
     }
 
+    @Test
+    void 이름은_5자_이하만_가능하다() {
+        String 여섯자리_이름 = "123456";
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(여섯자리_이름));
+    }
 }
